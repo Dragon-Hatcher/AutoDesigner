7d07a8f92f599e972c1223460237e867837156ad
Dragon-Hatcher <danieldragonhatcher@gmail.com>
2020-10-13 16:12:20 -0700

    Added New Look and Feel

--- src/extensions/TextFieldFilter.kt
+++ src/extensions/TextFieldFilter.kt
@@ -3,15 +3,20 @@ package extensions
 import javax.swing.JTextField
 import javax.swing.text.*
 
-fun JTextField.setFilter(test: (String) -> Boolean) {
-    (this.document as PlainDocument).documentFilter = TextFieldFilter(test)
+fun JTextField.setFilter(warnType: String? = null, test: (String) -> Boolean) {
+    (this.document as PlainDocument).documentFilter = TextFieldFilter(test, this, warnType)
 }
 
-internal class TextFieldFilter(val test: (String) -> Boolean) : DocumentFilter() {
+internal class TextFieldFilter(val test: (String) -> Boolean, val myTextField: JTextField, val possibleError: String?) : DocumentFilter() {
+
+    fun warn() {
+        myTextField.putClientProperty("JComponent.outline", possibleError)
+    }
 
     @Throws(BadLocationException::class)
     override fun insertString(fb: FilterBypass, offset: Int, string: String?,
                               attr: AttributeSet?) {
+
         val doc: Document = fb.document
         val sb = StringBuilder()
         sb.append(doc.getText(0, doc.length))
@@ -19,7 +24,7 @@ internal class TextFieldFilter(val test: (String) -> Boolean) : DocumentFilter()
         if (test(sb.toString())) {
             super.insertString(fb, offset, string, attr)
         } else {
-            // warn the user and don't allow the insert
+            warn()
         }
     }
 
@@ -34,7 +39,7 @@ internal class TextFieldFilter(val test: (String) -> Boolean) : DocumentFilter()
         if (test(sb.toString())) {
             super.replace(fb, offset, length, text, attrs)
         } else {
-            // warn the user and don't allow the insert
+            warn()
         }
     }
 
@@ -48,7 +53,7 @@ internal class TextFieldFilter(val test: (String) -> Boolean) : DocumentFilter()
         if (test(sb.toString())) {
             super.remove(fb, offset, length)
         } else {
-            // warn the user and don't allow the insert
+            warn()
         }
     }
 }
